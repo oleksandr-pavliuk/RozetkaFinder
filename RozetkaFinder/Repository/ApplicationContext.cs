eda02997d6958f6e296aa8c29f64aa776c6f537f
FoldeR-ua <61949378+FoldeR-ua@users.noreply.github.com>
2022-09-17 22:27:38 +0300

    Add Repository

---
+++ RozetkaFinder/Repository/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RozetkaFinder.Models.User;
+
+namespace RozetkaFinder.Repository
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=WIN-P3761FAKFQC;DataBase=UserDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ RozetkaFinder/Repository/UserRepository.cs
@@ -0,0 +1,68 @@
+﻿using RozetkaFinder.Models.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace RozetkaFinder.Repository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        void Create(User user);
+        Task<User> Read(string email);
+        Task<bool> Update(User user);
+        void Delete(User user);
+
+
+    }
+    public class UserRepository : IUserRepository                                      // CRUD operations
+    {
+        private readonly ApplicationContext _context;
+
+        public UserRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAll() => await _context.Users.ToListAsync();
+
+        //<------------ NEW USER ----------->
+        public async void Create(User user) 
+        {
+            await _context.Users.AddAsync(user);
+            _context.SaveChanges();
+        }
+
+
+        //<------------ SEARCH USER ----------->
+        public async Task<User> Read(string email)    
+        {
+            User user = await _context.Users.Where(u => u.Email == email).FirstOrDefaultAsync();
+            
+            return user;
+        }
+
+        //<--------------- UPDATE USER --------------->
+        public async Task<bool> Update(User user)   //modify user
+        {
+            User found = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            if (found == null)
+                return false;
+            else
+                _context.Entry(found).CurrentValues.SetValues(user);
+            
+            await _context.SaveChangesAsync();
+                 return true;
+            
+        }
+
+        //<------------------ DELETE USER ---------------->
+        public async void Delete(User user)   //delete user
+        {
+            _context.Users.Remove(user);
+           await _context.SaveChangesAsync();
+
+        }
+
+    }
+}
+
+
