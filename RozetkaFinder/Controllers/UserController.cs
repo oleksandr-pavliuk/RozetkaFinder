d1d8f701ee2bfd757db53ed28784607412271f11
FoldeR-ua <61949378+FoldeR-ua@users.noreply.github.com>
2022-09-17 22:29:19 +0300

    other

---
+++ RozetkaFinder/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RozetkaFinder.DTOs;
+using RozetkaFinder.Services.UserServices;
+
+namespace RozetkaFinder.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<TokenDTO>> RegisterAsync(UserRegisterDTO request) => await _userService.Create(request);
+
+
+
+
+    }
+}
--- RozetkaFinder/Program.cs
+++ RozetkaFinder/Program.cs
@@ -1,11 +1,33 @@
+using Microsoft.AspNetCore.Hosting.Builder;
+using RozetkaFinder.Services.Notification;
+using RozetkaFinder.Services.PasswordServices;
+using RozetkaFinder.Services.Security.JwtToken;
+using RozetkaFinder.Services.Security.RefreshToken;
+using RozetkaFinder.Repository;
+using AutoMapper;
+using RozetkaFinder.Models.Mapping;
+using RozetkaFinder.Services.IdServices;
+using RozetkaFinder.Services.UserServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationContext>();
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(AppMappingProfile));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IJwtService, JwtService>();
+builder.Services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+builder.Services.AddScoped<IPasswordService, PasswordService>();
+builder.Services.AddScoped<IIdService, IdService>();
+builder.Services.AddScoped<INotificationService, TelegramNotificationService>();
+builder.Services.AddScoped<INotificationService, EmailNotificationService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
