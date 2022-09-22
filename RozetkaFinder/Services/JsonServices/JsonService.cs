41192ff08293d7a490611d0cbe61d31f5f3eccd5
FoldeR-ua <61949378+FoldeR-ua@users.noreply.github.com>
2022-09-22 13:01:48 +0300

    Add Deserializing Json

--- RozetkaFinder/Services/JsonServices/JsonService.cs
+++ RozetkaFinder/Services/JsonServices/JsonService.cs
@@ -1,23 +1,40 @@
-﻿using RozetkaFinder.Models;
+﻿using Newtonsoft.Json;
 using System.Net;
+using RozetkaFinder.Models.GoodObjects;
+using AutoMapper;
 
 namespace RozetkaFinder.Services.JSONServices
 {
     public interface IJsonService
     {
-        Task<List<Good>> GetGoodsAsync(string name);
+        Task<List<GoodItem>> GetGoodsAsync(string name);
     }
     public class JsonService : IJsonService
     {
-        public async Task<List<Good>> GetGoodsAsync(string name)
+        private readonly IMapper _mapper;
+        public JsonService(IMapper mapper)
         {
-            List<Good> goods = new List<Good>()
+            _mapper = mapper;
+        }
+        public async Task<List<GoodItem>> GetGoodsAsync(string name)
+        {
+            using (WebClient wc = new WebClient())
             {
-                new Good() {Title ="Zenbook 14 UM", Id = 332135320, Link = "https://rozetka.com.ua/asus-90nb0sm1-m007m0/p332135320/", Price = 29999 },
-                new Good() {Title = "Zenbook 14 UX", Id =  351148491, Link = "https://rozetka.com.ua/asus-90nb0si1-m00a60/p351148491/", Price = 46999}
-            };
+                wc.Headers.Add("accept", "application/json, text/plain, */*");
+                wc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
 
-            return goods;
+                string uri = "https://search.rozetka.com.ua/ua/search/api/v6/?front-type=xl&country=UA&lang=ua&page=1&text=" + name.Replace(" ", "+");
+                string json = wc.DownloadString(uri);
+                Rootobject obj = JsonConvert.DeserializeObject<Rootobject>(json);
+
+                Good[] goods = obj.data.goods;
+                List<GoodItem> goodItems = new List<GoodItem>();
+                foreach(var item in goods)
+                {
+                    goodItems.Add(_mapper.Map<GoodItem>(item));
+                }
+                return goodItems; 
+            }
         }
     }
 }
